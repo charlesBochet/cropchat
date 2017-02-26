22ab9a2058dae8f7689b8635ff52d89652675aa6
Charles BOCHET <charlesb@theodo.fr>
2017-02-26 14:02:40 +0100

    Create empty views

---
+++ cropchat/src/components/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul class="list">
+  </ul>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .list {
+    width: 100%;
+    padding: 0;
+  }
+</style>
---
+++ cropchat/src/components/PostView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="waiting">
+    Not yet available
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+  .waiting {
+    padding: 10px;
+    color: #555;
+  }
+</style>
