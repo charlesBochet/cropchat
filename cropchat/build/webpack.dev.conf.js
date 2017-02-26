5ff77fd3cd71a988fad9c187d57e87ea80d670f0
Charles BOCHET <charlesb@theodo.fr>
2017-02-26 13:55:43 +0100

    Add Manifest file

--- cropchat/build/webpack.dev.conf.js
+++ cropchat/build/webpack.dev.conf.js
@@ -1,3 +1,4 @@
+var path = require('path')
 var utils = require('./utils')
 var webpack = require('webpack')
 var config = require('../config')
@@ -5,6 +6,7 @@ var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
+var manifesPlugin = require('pwa-manifest-webpack-plugin')
 
 // add hot-reload related code to entry chunks
 Object.keys(baseWebpackConfig.entry).forEach(function (name) {
@@ -18,6 +20,15 @@ module.exports = merge(baseWebpackConfig, {
   // cheap-module-eval-source-map is faster for development
   devtool: '#cheap-module-eval-source-map',
   plugins: [
+    new manifesPlugin({
+      name: 'CropChat',
+      description: 'CropChat - Image Messenger Application',
+      display: 'fullscreen',
+      icon: {
+        src: path.resolve('src/assets/logo.png'),
+        sizes: [200]
+      }
+    }),
     new webpack.DefinePlugin({
       'process.env': config.dev.env
     }),
--- cropchat/build/webpack.prod.conf.js
+++ cropchat/build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var manifesPlugin = require('pwa-manifest-webpack-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -27,6 +28,15 @@ var webpackConfig = merge(baseWebpackConfig, {
     chunkFilename: utils.assetsPath('js/[id].[chunkhash].js')
   },
   plugins: [
+    new manifesPlugin({
+      name: 'CropChat',
+      description: 'CropChat - Image Messenger Application',
+      display: 'fullscreen',
+      icon: {
+        src: path.resolve('src/assets/logo.png'),
+        sizes: [200]
+      }
+    }),
     // http://vuejs.github.io/vue-loader/en/workflow/production.html
     new webpack.DefinePlugin({
       'process.env': env
