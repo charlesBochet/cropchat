suits-at/vueJSPwa

Add firebase connection

cropchat/src/components/DetailView.vue
@@ -2,15 +2,15 @@
   <div class="mdl-grid">
     <div class="mdl-cell mdl-cell--8-col">
       <div class="picture">
-        <img :src="this.$data.pictures[$route.params.id].url" />
+        <img :src="this.cat.url" />
       </div>
       <div class="info">
-        <span>{{ this.$data.pictures[$route.params.id].info }}</span>
+        <span>{{ this.cat.info }}</span>
       </div>
     </div>
     <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
       <div class="comment">
-        <span>{{ this.$data.pictures[$route.params.id].comment }}</span>
+        <span>{{ this.cat.comment }}</span>
       </div>
       <div class="actions">
         <a class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" href="/#/post">
@@ -22,11 +22,17 @@
 </template>
 
 <script>
-import data from '../data'
+import firebase from '../service/firebase'
+
 export default {
+  mounted () {
+    firebase.database.ref('chat/' + this.$route.params.id).once('value').then(snapshot => {
+      this.cat = snapshot.val()
+    })
+  },
   data () {
     return {
-      'pictures': data.pictures
+      'cat': {}
     }
   }
 }
@@ -39,15 +45,6 @@ export default {
   }
 
   .info {
-  .card-image {
-    width: 100%;
-    height: 100%;
-  }
-  .card-image__picture > img {
-    color: #fff;
-    width:100%;
-  }
-  .card-image__info {
     text-align: right;
     padding: 5px;
     color: #555;
@@ -57,9 +54,5 @@ export default {
   .comment {
     padding: 10px;
     color: #555;
-  }
-
-  .actions {
-    text-align: center;
   }
 </style>

cropchat/src/components/PostView.vue
@@ -1,17 +1,71 @@
 <template>
-  <div class="waiting">
-    Not yet available
-  </div>
+  <form>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--8-col">
+        <div class="card-image__picture">
+          <div v-show="!isCatLoaded" class="mdl-spinner mdl-js-spinner is-active"></div>
+          <img :src="this.catUrl"/>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
+          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
+          <label for="username" class="mdl-textfield__label">Describe me</label>
+        </div>
+        <div class="actions">
+          <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            POST A CAT
+          </a>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
+import parse from 'xml-parser'
+
 export default {
+  data () {
+    return {
+      'catUrl': '',
+      'title': ''
+    }
+  },
+  computed: {
+    isCatLoaded: function () {
+      return this.catUrl !== ''
+    }
+  },
+  mounted () {
+    this.$http.get('http://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+      this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+    })
+  },
+  methods: {
+    postCat () {
+      this.$root.$firebaseRefs.chat.push(
+        {
+          'url': this.catUrl,
+          'comment': this.title,
+          'info': 'Posted by Charles on Tuesday',
+          'created_at': -1 * new Date().getTime()
+        }
+      ).then(
+        this.$router.push('/')
+      )
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .waiting {
-    padding: 10px;
-    color: #555;
+  img {
+    width: 100%;
+  }
+  .mdl-spinner {
+    position: relative;
+    top: 50%;
+    left: 50%;
   }
 </style>

cropchat/src/main.js
@@ -3,10 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuefire from 'vuefire'
+import firebase from './service/firebase'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(Vuefire)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  firebase: {
+    chat: firebase.database.ref('chat').orderByChild('created_at')
+  },
   router,
   template: '<App/>',
   components: { App }

charlesBochet/cropchat

cropchat/src/components/DetailView.vue
@@ -2,15 +2,15 @@
   <div class="mdl-grid">
     <div class="mdl-cell mdl-cell--8-col">
       <div class="picture">
-        <img :src="this.pictures[$route.params.id].url" />
+          <img :src="this.cat.url" />
       </div>
       <div class="info">
-        <span>{{ this.pictures[$route.params.id].info }}</span>
+        <span>{{ this.cat.info }}</span>
       </div>
     </div>
     <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
       <div class="comment">
-        <span>{{ this.pictures[$route.params.id].comment }}</span>
+        <span>{{ this.cat.comment }}</span>
       </div>
       <div class="actions">
         <a class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" href="/#/post">
@@ -22,11 +22,17 @@
 </template>
 
 <script>
-import data from '../data'
+import firebase from '../service/firebase'
+
 export default {
+  mounted () {
+    firebase.database.ref('cat/' + this.$route.params.id).once('value').then(snapshot => {
+      this.cat = snapshot.val()
+    })
+  },
   data () {
     return {
-      'pictures': data.pictures
+      'cat': {}
     }
   }
 }
@@ -39,15 +45,6 @@ export default {
   }
 
   .info {
-  .card-image {
-    width: 100%;
-    height: 100%;
-  }
-  .card-image__picture > img {
-    color: #fff;
-    width:100%;
-  }
-  .card-image__info {
     text-align: right;
     padding: 5px;
     color: #555;
@@ -57,9 +54,5 @@ export default {
   .comment {
     padding: 10px;
     color: #555;
-  }
-
-  .actions {
-    text-align: center;
   }
 </style>

cropchat/src/components/PostView.vue
@@ -1,17 +1,71 @@
 <template>
-  <div class="waiting">
-    Not yet available
-  </div>
+  <form>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--8-col">
+        <div class="card-image__picture">
+          <div v-show="!isCatLoaded" class="mdl-spinner mdl-js-spinner is-active"></div>
+          <img :src="this.catUrl"/>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
+          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
+          <label for="username" class="mdl-textfield__label">Describe me</label>
+        </div>
+        <div class="actions">
+          <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            POST A CAT
+          </a>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 
 <script>
+import parse from 'xml-parser'
+
 export default {
+  data () {
+    return {
+      'catUrl': '',
+      'title': ''
+    }
+  },
+  computed: {
+    isCatLoaded: function () {
+      return this.catUrl !== ''
+    }
+  },
+  mounted () {
+    this.$http.get('http://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+      this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+    })
+  },
+  methods: {
+    postCat () {
+      this.$root.$firebaseRefs.cat.push(
+        {
+          'url': this.catUrl,
+          'comment': this.title,
+          'info': 'Posted by Charles on Tuesday',
+          'created_at': -1 * new Date().getTime()
+        }
+      ).then(
+        this.$router.push('/')
+      )
+    }
+  }
 }
 </script>
 
 <style scoped>
-  .waiting {
-    padding: 10px;
-    color: #555;
+  img {
+    width: 100%;
+  }
+  .mdl-spinner {
+    position: relative;
+    top: 50%;
+    left: 50%;
   }
 </style>

cropchat/src/main.js
@@ -3,10 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Vuefire from 'vuefire'
+import firebase from './service/firebase'
+import VueResource from 'vue-resource'
+
+Vue.use(VueResource)
+Vue.use(Vuefire)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  firebase: {
+    cat: firebase.database.ref('cat').orderByChild('created_at')
+  },
   router,
   template: '<App/>',
   components: { App }