8986bb75678df346b7797c2224d70119409abf82
Mahdi Bolow <boolow5@gmail.com>
2017-05-25 12:21:11 +0300

    setup all plugins to work

--- cropchat/src/main.js
+++ cropchat/src/main.js
@@ -3,13 +3,32 @@
 import Vue from 'vue'
 import App from './App'
 // import router from './router'
-import Vuefire from 'vuefire'
+// import Vuefire from 'vuefire'
 import store from './store'
 // import firebase from './service/firebase'
 import VueResource from 'vue-resource'
+import vuexI18n from 'vuex-i18n'
+import VueEvents from 'vue-events'
+
+// import translations
+import Spanish from '../translations/locales/es'
+import French from '../translations/locales/fr'
+import Arabic from '../translations/locales/ar'
+
+require('expose-loader?$!expose-loader?jQuery!jquery')
+require('bootstrap-webpack')
+require('font-awesome/fonts/fontawesome-webfont.svg')
+require('../static/font-awesome-8500ab3a5a.js')
+
+Vue.use(vuexI18n.plugin, store)
+Vue.i18n.add('es', Spanish)
+Vue.i18n.add('fr', French)
+Vue.i18n.add('ar', Arabic)
 
 Vue.use(VueResource)
-Vue.use(Vuefire)
+// Vue.use(Vuefire)
+
+Vue.use(VueEvents)
 
 /* eslint-disable no-new */
 new Vue({
