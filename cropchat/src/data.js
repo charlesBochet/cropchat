257080f0f2e447ff8e0c47dab67b8c12eb6c8e6d
Charles BOCHET <charlesb@theodo.fr>
2017-06-01 23:40:08 +0200

    Populate and style views

---
+++ cropchat/src/data.js
@@ -0,0 +1,34 @@
+export default {
+  pictures: [
+    {
+      'id': 0,
+      'url': 'https://25.media.tumblr.com/tumblr_m40h4ksiUa1qbyxr0o1_400.gif',
+      'comment': 'A cat game',
+      'info': 'Posted by Kevin on Friday'
+    },
+    {
+      'id': 1,
+      'url': 'https://25.media.tumblr.com/tumblr_lhd7n9Qec01qgnva2o1_500.jpg',
+      'comment': 'Tatoo & cat',
+      'info': 'Posted by Charles on Tuesday'
+    },
+    {
+      'id': 2,
+      'url': 'https://24.media.tumblr.com/tumblr_m4j2atctRm1qejbiro1_1280.jpg',
+      'comment': 'Santa cat',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 3,
+      'url': 'https://25.media.tumblr.com/tumblr_m3rmbwhVB51qhwmnpo1_1280.jpg',
+      'comment': 'Mexico cat',
+      'info': 'Posted by Richard on Monday'
+    },
+    {
+      'id': 4,
+      'url': 'https://24.media.tumblr.com/tumblr_mceknxs4Lo1qd477zo1_500.jpg',
+      'comment': 'Curious cat',
+      'info': 'Posted by Richard on Monday'
+    }
+  ]
+}
