310f75895333a9f60cfb2b3bf343ea9a185b3a51
Charles BOCHET <charlesb@theodo.fr>
2017-06-01 23:36:50 +0200

    Create views skeleton and add Material Design Lite

---
+++ cropchat/src/components/PostView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="waiting">
+    Not yet available
+  </div>
+</template>
+<script>
+  export default {
+  }
+</script>
+<style scoped>
+  .waiting {
+    padding: 10px;
+    color: #555;
+  }
+</style>
