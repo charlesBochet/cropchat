8c0996f6047af2fc9b7e579d37be42773251bf7a
Kevin <kevinj@theodo.fr>
2017-09-24 17:43:55 +0200

    Add reload button

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -1,19 +1,23 @@
 <template>
   <form>
     <div class="mdl-grid">
-      <div class="mdl-cell mdl-cell--8-col">
-        <div class="card-image__picture">
-          <img :src="this.catUrl"/>
-        </div>
+      <div class="mdl-cell mdl-cell--8-col image-container">
+      <transition-group name="fade">
+        <div key="1" v-if="isImageLoading" class="mdl-spinner mdl-js-spinner is-active"></div>
+        <img key="2" v-if="!isImageLoading" class="cat-image" v-show="catUrl !== null" :src="this.catUrl"/>
+      </transition-group>
       </div>
-      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+      <div class="mdl-cell">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
           <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
           <label for="username" class="mdl-textfield__label">Describe me</label>
         </div>
         <div class="actions">
           <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-            POST A CAT
+            <span>POST IMAGE</span>
+          </a>
+          <a @click.prevent="loadCatImageFromCatAPI" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            <i class="material-icons">refresh</i>
           </a>
         </div>
       </div>
@@ -22,22 +26,17 @@
 </template>
 <script>
   import parse from 'xml-parser'
+
   export default {
     data () {
       return {
-        'catUrl': '',
+        'catUrl': null,
+        'isImageLoading': true,
         'title': ''
       }
     },
-    computed: {
-      isCatLoaded: function () {
-        return this.catUrl !== ''
-      }
-    },
     mounted () {
-      this.$http.get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
-        this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
-      })
+      this.loadCatImageFromCatAPI()
     },
     methods: {
       postCat () {
@@ -51,17 +50,37 @@
         ).then(
           this.$router.push('/')
         )
+      },
+      loadCatImageFromCatAPI () {
+        this.isImageLoading = true
+        this.$http.get('https://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+          const catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+          let img = new Image()
+          img.src = catUrl
+          img.onload = () => {
+            this.isImageLoading = false
+            this.catUrl = catUrl
+          }
+        })
       }
     }
   }
 </script>
+
 <style scoped>
   img {
     width: 100%;
   }
-  .mdl-spinner {
-    position: relative;
-    top: 50%;
-    left: 50%;
+  .image-container {
+    min-height: 280px;
+    min-width: 280px;
+    justify-content: center;
+    display: flex;
+    align-items: center;
+  }
+  .cat-image {
+    max-height: 280px;
+    max-width: 100%;
+    width: auto;
   }
 </style>
