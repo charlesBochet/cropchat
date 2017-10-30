66573e4ceb45ed78e8127b7bf28700a4f6401f00
Charles BOCHET <charlesb@theodo.fr>
2017-10-30 22:30:13 +0100

    Cache app shell and cat images

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,7 +106,32 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/code\.getmdl\.io\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/thecatapi\.com\/api\/images\/get\.php\?id/,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^https:\/\/(\d+)\.media\.tumblr\.com\//,
+        handler: 'cacheFirst'
+      },
+      {
+        urlPattern: /^http:\/\/(\d+)\.media\.tumblr\.com\//,
+        handler: 'cacheFirst'
+      }]
     })
   ]
 })
