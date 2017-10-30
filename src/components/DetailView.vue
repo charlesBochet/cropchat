cf1e7ce7467fb36376aaedd52553b0896b0e8b58
Charles BOCHET <charlesb@theodo.fr>
2017-10-30 21:24:50 +0100

    Populate DetailView

--- src/components/DetailView.vue
+++ src/components/DetailView.vue
@@ -1,10 +1,51 @@
 <template>
-  <div class="card-image">
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--8-col">
+      <div class="picture">
+        <img :src="this.pictures[$route.params.id].url" />
+      </div>
+      <div class="info">
+        <span>{{ this.pictures[$route.params.id].info }}</span>
+      </div>
+    </div>
+    <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+      <div class="comment">
+        <span>{{ this.pictures[$route.params.id].comment }}</span>
+      </div>
+      <div class="actions">
+        <router-link class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" to="/post">
+          ANSWER
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+  import data from '../data'
   export default {
+    data () {
+      return {
+        'pictures': data.pictures
+      }
+    }
   }
 </script>
 <style scoped>
+  .picture > img {
+    color: #fff;
+    width:100%;
+  }
+  .info {
+    text-align: right;
+    padding: 5px;
+    color: #555;
+    font-size: 10px;
+  }
+  .comment {
+    padding: 10px;
+    color: #555;
+  }
+  .actions {
+    text-align: center;
+  }
 </style>
