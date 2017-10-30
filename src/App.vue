84ba30fc8cae73d8990ef2e197afa3c5c778de94
Charles BOCHET <charlesb@theodo.fr>
2017-10-30 21:09:31 +0100

    Add Material Navigation

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,40 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  require('material-design-lite')
+
+  export default {
+    name: 'app',
+    methods: {
+      hideMenu: function () {
+        document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+        document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+      }
+    }
+  }
 </script>
 
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
+  @import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+  @import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
 </style>
