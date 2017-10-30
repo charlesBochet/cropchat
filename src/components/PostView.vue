cd004b25882e626a348c82a63903e95e0cd7ca55
Charles BOCHET <charlesb@theodo.fr>
2017-10-30 22:02:03 +0100

    Add post feature

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -1,10 +1,51 @@
 <template>
-  <div class="waiting">
-    Not yet available
-  </div>
+  <form>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--8-col">
+        <div class="card-image__picture">
+          <img :src="this.catUrl"/>
+        </div>
+      </div>
+      <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
+        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
+          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
+          <label for="username" class="mdl-textfield__label">Describe me</label>
+        </div>
+        <div class="actions">
+          <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            POST A CAT
+          </a>
+        </div>
+      </div>
+    </div>
+  </form>
 </template>
 <script>
+  import parse from 'xml-parser'
+
   export default {
+    data () {
+      return {
+        'catUrl': null
+      }
+    },
+    mounted () {
+      this.$http.get('http://thecatapi.com/api/images/get?format=xml&results_per_page=1').then(response => {
+        this.catUrl = parse(response.body).root.children['0'].children['0'].children['0'].children['0'].content
+      })
+    },
+    methods: {
+      postCat () {
+        this.$root.$firebaseRefs.cat.push(
+          {
+            'url': this.catUrl,
+            'comment': this.title,
+            'info': 'Posted by Charles on Tuesday',
+            'created_at': -1 * new Date().getTime()
+          })
+          .then(this.$router.push('/'))
+      }
+    }
   }
 </script>
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import Vuefire from 'vuefire'
+import VueResource from 'vue-resource'
 import App from './App'
 import router from './router'
 import firebase from '@/services/firebase'
 
 Vue.use(Vuefire)
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
