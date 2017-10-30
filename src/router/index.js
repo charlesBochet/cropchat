3512430fec3b5f5492eaa2fe3ba8468713f9c5f8
Charles BOCHET <charlesb@theodo.fr>
2017-10-30 20:48:37 +0100

    Create app skeleton

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import HomeView from '@/components/HomeView'
+import DetailView from '@/components/DetailView'
+import PostView from '@/components/PostView'
 
 Vue.use(Router)
 
@@ -8,8 +10,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/detail/:id',
+      name: 'detail',
+      component: DetailView
+    },
+    {
+      path: '/post',
+      name: 'post',
+      component: PostView
     }
   ]
 })
