c3cd5c905c14d078165900bc3f8640e60196b6ff
Kevin <kevinj@theodo.fr>
2018-07-17 09:46:34 +0200

    Add push notification

--- build/service-worker-prod.js
+++ build/service-worker-prod.js
@@ -14,42 +14,19 @@
     );
 
   window.addEventListener('load', function() {
-      if ('serviceWorker' in navigator &&
-          (window.location.protocol === 'https:' || isLocalhost)) {
-        navigator.serviceWorker.register('service-worker.js')
-        .then(function(registration) {
-          // updatefound is fired if service-worker.js changes.
-          registration.onupdatefound = function() {
-            // updatefound is also fired the very first time the SW is installed,
-            // and there's no need to prompt for a reload at that point.
-            // So check here to see if the page is already controlled,
-            // i.e. whether there's an existing service worker.
-            if (navigator.serviceWorker.controller) {
-              // The updatefound event implies that registration.installing is set
-              const installingWorker = registration.installing;
-
-              installingWorker.onstatechange = function() {
-                switch (installingWorker.state) {
-                  case 'installed':
-                    // At this point, the old content will have been purged and the
-                    // fresh content will have been added to the cache.
-                    // It's the perfect time to display a "New content is
-                    // available; please refresh." message in the page's interface.
-                    break;
-
-                  case 'redundant':
-                    throw new Error('The installing ' +
-                                    'service worker became redundant.');
-
-                  default:
-                    // Ignore
-                }
-              };
-            }
-          };
-        }).catch(function(e) {
-          console.error('Error during service worker registration:', e);
+    if('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/service-worker .js', { scope: '/' }).then(function() {
+        return navigator.serviceWorker.ready;
+      }).then(function(registration) {
+        registration.pushManager.subscribe({userVisibleOnly: true}).then(function(sub) {
+          var endpointSections = sub.endpoint.split('/');
+          var subscriptionId = endpointSections[endpointSections.length - 1];
+          console.log('endpoint:', subscriptionId);
         });
-      }
+      });
+      navigator.serviceWorker.ready.then(function(registration) {
+        console.log('Service Worker Ready');
+      });
+    }
   });
 })();
